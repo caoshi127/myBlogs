774faaaee2572ff5646ce819346cf02768656318
caishi127 <2088953828@qq.com>
2019-12-06 11:22:12 +0800

    解决登录bug 2019年12月6日11:21:32

--- src/main/java/com/caoshi/myblogs/controller/LoginController.java
+++ src/main/java/com/caoshi/myblogs/controller/LoginController.java
@@ -3,10 +3,15 @@ package com.caoshi.myblogs.controller;
 import com.caoshi.myblogs.constant.ResultObj;
 import com.caoshi.myblogs.domain.User;
 import com.caoshi.myblogs.service.UserService;
+import com.caoshi.myblogs.util.CheckCodeUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * 登录控制器
@@ -14,27 +19,63 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @time 2019/12/3 18:15
  */
 @Controller
-@RequestMapping("login")
+@RequestMapping("/login")
 public class LoginController {
 
     @Autowired
     private UserService userService;
 
-    @RequestMapping("toLogin")
+    /**
+     * 跳转到登录页面
+     * @return
+     */
+    @RequestMapping("/toLogin")
     public String toLogin() {
         return "/system/login";
     }
 
-    @RequestMapping("login")
+    /**
+     *
+     * @param username 用户输入的用户名
+     * @param password 用户输入的密码
+     * @param vcode 用户输入的验证码
+     * @param request HttpServletRequest
+     * @return
+     */
+    @RequestMapping("/login")
     @ResponseBody
-    public ResultObj login(User user) {
-        User loginUser = userService.userLogin(user);
-        if (null != loginUser) {
+    public ResultObj login(String username, String password, String vcode, HttpServletRequest request) {
+
+        System.out.println("LoginController.login" + username + "--" + password + "--" + vcode);
+
+        User loginUser = new User(username, password);
+        User user = userService.userLogin(loginUser);
+        String vcodeStr = request.getSession().getAttribute("CHECKCODE_SESSION").toString();
+        if (null != user && vcode.equalsIgnoreCase(vcodeStr)) {
             return ResultObj.LOGIN_SUCCESS;
         } else {
+
+            System.out.println();
             return ResultObj.LOGIN_ERROR;
+
         }
     }
+
+    /**
+     * 获取验证码
+     * @param req  HttpServletRequest
+     * @param resp HttpServletResponse
+     */
+    @RequestMapping("/code")
+    public void getCode(HttpServletRequest req, HttpServletResponse resp) {
+        CheckCodeUtil checkCodeUtil = new CheckCodeUtil();
+        try {
+            checkCodeUtil.writeCheckCode(req, resp);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 
 }
--- src/main/java/com/caoshi/myblogs/domain/User.java
+++ src/main/java/com/caoshi/myblogs/domain/User.java
@@ -1,52 +1,29 @@
 package com.caoshi.myblogs.domain;
 
+import lombok.*;
+
 import java.util.Date;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
 public class User {
 
     private Integer id;
+
     private String userName;
+
     private String password;
+
     private Date regTime;
+
     private String qq;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
+    public User(String userName, String password) {
         this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
     }
 
-    public Date getRegTime() {
-        return regTime;
-    }
-
-    public void setRegTime(Date regTime) {
-        this.regTime = regTime;
-    }
-
-    public String getQq() {
-        return qq;
-    }
-
-    public void setQq(String qq) {
-        this.qq = qq;
-    }
 }
